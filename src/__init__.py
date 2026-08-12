0b3a52b438fcc83d33fa35610c63308c721acf5e
TheYasterix <yasterix05@gmail.com>
2026-08-11 20:48:18 -0600

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""
+Juridico Compliance Framework - módulo de inicialización.
+
+Este archivo prepara el paquete para futuras extensiones
+relacionadas con análisis normativo y generación de documentos legales.
+"""
